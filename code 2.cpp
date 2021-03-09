9c0abdb1f08f93063c1e494599232c182c603790
Sabreen535 <80199961+Sabreen535@users.noreply.github.com>
2021-03-09 17:36:37 +0500

    Now understand else if statement

---
+++ code 2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	printf("\nEnter a number:");
+	scanf("%d",&n);
+	if(n>0)
+	printf("\nThe square of %d is %d",n,n*n);
+	
+	
+
+}
+
